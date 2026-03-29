5be3aa7a87dd1edf167a0a2a0f61265fbfedaf7d
minhtb105 <tranbinhminh_t64@hus.edu.vn>
2026-03-29 10:30:12 +0700

    Remove demo/ folder - superseded by src/

--- demo/src/templates/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-CV Template definitions and styling system
-"""
-from .template_manager import TemplateManager, TemplateStyle, TemplateConfig
-from .converter import PDFToDocxConverter
-
-__all__ = ["TemplateManager", "TemplateStyle", "TemplateConfig", "PDFToDocxConverter"]
